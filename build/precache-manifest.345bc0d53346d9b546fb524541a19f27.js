62f732b9fffe1530a4d4572bafaf00e97d5aaad0
Janscode <55261974+Janscode@users.noreply.github.com>
2020-05-26 17:06:35 -0600

    got rid of redundant directory

---
+++ build/precache-manifest.345bc0d53346d9b546fb524541a19f27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d488d2cf3eba1698ba00af5fa054bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7ca402727389be5a123",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1e8e06c1f8123b7aab5d",
+    "url": "/static/js/2.89763ab4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.89763ab4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7ca402727389be5a123",
+    "url": "/static/js/main.65c64afb.chunk.js"
+  },
+  {
+    "revision": "231224f21a7d13766a04",
+    "url": "/static/js/runtime-main.5edcc308.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
